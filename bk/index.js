58c27f2f4841e593967dd3982a8b5de3115e4170
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2024-10-20 11:02:25 +0530

    main

---
+++ bk/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+dotenv.config({
+    path:'./env'
+})
+
+const port = process.env.PORT || 5643
+
+const app = express();
+
+app.use(express.urlencoded(
+    {
+        limit:"16Kb",
+        extended:true
+    }
+));
+app.use(cors(
+    {
+    options:process.env.CORS,
+    credentials:true
+    }
+));
+app.use(cookieParser())
+app.use((express.json({
+    limit:"16Kb"
+})))
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to the server made by express")
+});
+
+(async()=>{
+    try {
+        const connectionInstances = await mongoose.connect(`${process.env.MONGO_URL}/Blog`);
+        console.log("Server is connected with the database Successfully")
+
+        app.on('error', (error) => {
+            console.log(`Server error:`, error);
+        });
+        
+        app.listen(port, () => {
+            console.log(`Your server is running on port: ${port}`);
+        });
+    } catch (error) {
+        console.log(`Something went wrong during the connection to the database`);
+        console.log(`Check::`, error);
+    }
+})();
+
