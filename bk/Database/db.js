c0dff80f6ad170fda42751d7290ac0f5b76f38cf
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2025-07-20 06:22:30 +0530

    code correction

--- bk/Database/db.js
+++ bk/Database/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const database = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URL}`, {
-      dbName: "SocialMedia",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(`mongodb+srv://Nikhil:123yougetfree@cluster0.kz2acay.mongodb.net/SocialMedia`);
     console.log("✅ Server is connected with the database successfully");
   } catch (error) {
     console.error("❌ Failed to connect to database:", error);
